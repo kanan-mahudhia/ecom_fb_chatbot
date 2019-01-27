8019cd4b90281fc70f5fedab9563eb8cd86c8e60
kananmahudhia <kananmahudhia@cosaia.com>
2019-01-27 11:50:19 +0530

    Added: User verify API

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var request = require("request");
 var PORT = process.env.PORT || 5000;
 
 var app = express();
@@ -11,6 +12,30 @@ app.use(bodyParser.json());
 app.get("/", function (req, res) {
   res.send("Deployed!");
 });
+
+request.post({
+    "headers": { "content-type": "application/json" },
+    "url": "http://gtps.org.in/shoppify/user/verify/",
+    "body": JSON.stringify({
+        "fbid": "123"
+    })
+}, (error, response, body) => {
+    if(error) {
+        return console.dir(error);
+    }
+    console.dir(JSON.parse(body));
+});
+
+//request.post({
+//    "headers": { "content-type": "application/x-www-form-urlencoded" },
+//    "url": "http://gtps.org.in/shoppify/user/verify/",
+//    "body": "fbid=132"
+//}, (error, response, body) => {
+//    if(error) {
+//        return console.dir(error);
+//    }
+//    console.dir(JSON.parse(body));
+//});
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
