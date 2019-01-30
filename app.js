96c42f9a738e921cec315bda9c3a2524bd29695d
kananmahudhia <kananmahudhia@cosaia.com>
2019-01-30 21:17:21 +0530

    updated: get started btn

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get("/", function (req, res) {
 // Facebook Webhook
 // Used for verification
 app.get("/webhook", function (req, res) {
-    if (req.query["hub.verify_token"] === "ecom_token") {
+    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
         console.log("Verified webhook");
         res.status(200).send(req.query["hub.challenge"]);
     } else {
@@ -24,6 +24,75 @@ app.get("/webhook", function (req, res) {
         res.sendStatus(403);
     }
 });
+
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", function (req, res) {
+    // Make sure this is a page subscription
+    // console.log(req.body.entry[0].messaging[0].message.text);
+    if (req.body.object == "page") {
+        // Iterate over each entry
+        // There may be multiple entries if batched
+        req.body.entry.forEach(function (entry) {
+            // Iterate over each messaging event
+            entry.messaging.forEach(function (event) {
+                if (event.postback) {
+                    processPostback(event);
+                } else if (event.message) {
+                    processMessage(event);
+                }
+            });
+        });
+
+        res.sendStatus(200);
+    }
+});
+
+function processPostback(event) {
+    var senderId = event.sender.id;
+    var payload = event.postback.payload;
+    console.log("-------------------->" + event.postback.payload);
+    if (payload === "GET_STARTED_PAYLOAD") {
+        // Get user's first name from the User Profile API
+        // and include it in the greeting
+        state = 0;
+        request({
+            url: "https://graph.facebook.com/v2.6/" + senderId,
+            qs: {
+                access_token: process.env.PAGE_ACCESS_TOKEN,
+                fields: "first_name"
+            },
+            method: "GET"
+        }, function (error, response, body) {
+            var greeting = "";
+            if (error) {
+                console.log("Error getting user's name: " + error);
+            } else {
+                var bodyObj = JSON.parse(body);
+                name = bodyObj.first_name;
+                greeting = "Hello " + name + ". ";
+            }
+
+            sendMessage(senderId, { text: greeting });
+        });
+    }
+
+}
+
+function processMessage(event) {
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        method: "POST",
+        json: {
+            recipient: { id: recipientId },
+            message: message,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log("Error sending message: " + response.error);
+        }
+    });
+}
 
 
 request.post({
